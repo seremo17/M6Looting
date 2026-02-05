34df0ef561c8e443af333ffd1c7a0e23a91623c2
seremo17 <seremo@s.sfusd.edu>
2026-02-05 13:28:29 -0800

    M6L5 Completed
    M6L5 Completed

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
@@ -19,3 +19,20 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
+
+func open() -> void:
+	animation_player.play("open")
+
+
+
+	input_pickable= false

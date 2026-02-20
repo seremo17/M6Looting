eb2384eb646086d4ba99700e5287a7f1b6e36b92
seremo17 <seremo@s.sfusd.edu>
2026-02-20 10:55:48 -0800

    M6L6 Completed
    M6L6 Completed

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+@export var possible_items: Array[PackedScene] = []
+
 @onready var canvas_group: CanvasGroup = $CanvasGroup
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
@@ -10,11 +13,9 @@ func _ready() -> void:
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
 
-
 func _on_mouse_entered() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 3.0, 6.0, 0.08)
-
 
 func _on_mouse_exited() -> void:
 	var tween := create_tween()
@@ -22,7 +23,7 @@ func _on_mouse_exited() -> void:
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
 	var event_is_mouse_click: bool = (
-		event is InputEventMouseButton and 
+		event is InputEventMouseButton and
 		event.button_index == MOUSE_BUTTON_LEFT and
 		event.is_pressed()
 	)
@@ -33,6 +34,19 @@ func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
 func open() -> void:
 	animation_player.play("open")
 
+	input_pickable = false
 
+	if possible_items.is_empty():
+		return
 
-	input_pickable= false
+	for current_index in range(randi_range(1, 3)):
+		_spawn_random_item()
+
+func _spawn_random_item() -> void:
+	var loot_item: Area2D = possible_items.pick_random().instantiate()
+	add_child(loot_item)
+
+	var random_angle := randf_range(0.0, 2.0 * PI)
+	var random_direction := Vector2(1.0, 0.0).rotated(random_angle)
+	var random_distance := randf_range(60.0, 120.0)
+	loot_item.position = random_direction * random_distance
